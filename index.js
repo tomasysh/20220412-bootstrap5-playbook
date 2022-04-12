9080ef9c2b052ca430e80b060b5429df627e3aca
tomasysh <tomasysh@gmail.com>
2022-04-12 17:31:21 +0800

    8. evernote - navbar shadow

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 $(document).ready(function() {
+    const $navbar = $('#navbar');
     const $cookieHintContainer = $('#cookie-hint-container');
-    $(document).on('click', '#cookie-hint-button', function() {
-        // $cookieHintContainer.addClass('d-none');
-        $cookieHintContainer.fadeOut();
-    })
+    $(document)
+        .on('click', '#cookie-hint-button', function() {
+            // $cookieHintContainer.addClass('d-none');
+            $cookieHintContainer.fadeOut();
+        })
+        .on('scroll', function() {
+            // if ($navbar.offset().top > 80) {
+            //     $navbar.addClass('shadow');
+            // } else {
+            //     $navbar.removeClass('shadow');
+            // }
+            $navbar.offset().top > 80
+                ? $navbar.addClass('shadow')
+                : $navbar.removeClass('shadow');
+        });
 
 });
